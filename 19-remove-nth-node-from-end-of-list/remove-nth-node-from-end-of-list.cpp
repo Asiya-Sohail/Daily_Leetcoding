0190d0d6089dc926390c1274e8f7a4c844c58ecc
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-11-16 00:13:53 +0500

    Time: 0 ms (100.00%) | Memory: 14.5 MB (99.16%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -11,38 +11,57 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* next = nullptr;
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while(curr != nullptr) {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        int count = 1;
-        ListNode* temp = prev;
-        ListNode* y = nullptr;
-        while (count != n && temp != nullptr) {
-            y = temp;
-            temp = temp->next;
-            count++;
-        }
-        if (count == n) {
-            if (y == nullptr)
-                prev = temp->next;
-            else
-                y->next = y->next->next;
+        ListNode* temp = new ListNode(0, head);
+        ListNode* fast = temp;
+        ListNode* slow = temp;
+        
+        for (int i=0; i<=n; i++) {
+            fast = fast->next;
         }
 
-        curr = prev;
-        prev=  nullptr;
-        while(curr != nullptr) {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
+        while (fast != nullptr) {
+            slow = slow->next;
+            fast = fast->next;
         }
-        return prev;
+
+        slow->next = slow->next->next;
+        return temp->next;
     }
+
+    //MY OWN SOLUTION
+    // ListNode* removeNthFromEnd(ListNode* head, int n) {
+    //     ListNode* next = nullptr;
+    //     ListNode* prev = nullptr;
+    //     ListNode* curr = head;
+    //     while(curr != nullptr) {
+    //         next = curr->next;
+    //         curr->next = prev;
+    //         prev = curr;
+    //         curr = next;
+    //     }
+    //     int count = 1;
+    //     ListNode* temp = prev;
+    //     ListNode* y = nullptr;
+    //     while (count != n && temp != nullptr) {
+    //         y = temp;
+    //         temp = temp->next;
+    //         count++;
+    //     }
+    //     if (count == n) {
+    //         if (y == nullptr)
+    //             prev = temp->next;
+    //         else
+    //             y->next = y->next->next;
+    //     }
+
+    //     curr = prev;
+    //     prev=  nullptr;
+    //     while(curr != nullptr) {
+    //         next = curr->next;
+    //         curr->next = prev;
+    //         prev = curr;
+    //         curr = next;
+    //     }
+    //     return prev;
+    // }
 };
