842f437ff6a50035b7e95061a70852205812ec7f
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-11-25 18:58:29 +0500

    Time: 7 ms (43.70%) | Memory: 23.9 MB (25.44%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
-    void merge(vector<vector<int>>& arr, int l, int mid, int h) {
-        int n1 = mid - l + 1;
-        int n2 = h - mid;
-        vector<vector<int>> L(n1), R(n2);
-        for (int i=0; i<n1; i++) 
-            L[i] = arr[l + i];
-        for (int i=0; i<n2; i++) 
-            R[i] = arr[mid + 1 + i];
-        int i=0, j=0, k=l;
-        while(i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
-                i++; k++;
-            } else {
-                arr[k] = R[j];
-                j++; k++;
-            }
-        }
-        while (i < n1) {
-            arr[k] = L[i];
-            i++; k++;
-        }
-        while (j < n2) {
-            arr[k] = R[j];
-            j++; k++;
-        }
-    }
+//     void merge(vector<vector<int>>& arr, int l, int mid, int h) {
+//         int n1 = mid - l + 1;
+//         int n2 = h - mid;
+//         vector<vector<int>> L(n1), R(n2);
+//         for (int i=0; i<n1; i++) 
+//             L[i] = arr[l + i];
+//         for (int i=0; i<n2; i++) 
+//             R[i] = arr[mid + 1 + i];
+//         int i=0, j=0, k=l;
+//         while(i < n1 && j < n2) {
+//             if (L[i] <= R[j]) {
+//                 arr[k] = L[i];
+//                 i++; k++;
+//             } else {
+//                 arr[k] = R[j];
+//                 j++; k++;
+//             }
+//         }
+//         while (i < n1) {
+//             arr[k] = L[i];
+//             i++; k++;
+//         }
+//         while (j < n2) {
+//             arr[k] = R[j];
+//             j++; k++;
+//         }
+//     }
     
-    void mergesort(vector<vector<int>>& arr, int l, int h) {
-        if (l == h) return;
-        int mid = l + (h-l) / 2;
-        mergesort(arr, l, mid);
-        mergesort(arr, mid+1, h);
-        merge(arr, l, mid, h);
-    }
+//     void mergesort(vector<vector<int>>& arr, int l, int h) {
+//         if (l == h) return;
+//         int mid = l + (h-l) / 2;
+//         mergesort(arr, l, mid);
+//         mergesort(arr, mid+1, h);
+//         merge(arr, l, mid, h);
+//     }
     
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n = intervals.size();
         vector<vector<int>> ans;
-        mergesort(intervals, 0, n-1);
+        // mergesort(intervals, 0, n-1);
+        sort(intervals.begin(), intervals.end());
         for (int i=0; i<n; i++) {
             int l = intervals[i][0];
             int h = intervals[i][1];
