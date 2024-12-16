5c956a9665401c8933285dcabcfb86b453bc0b7e
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-16 19:17:05 +0500

    Time: 35 ms (46.13%) | Memory: 73.5 MB (52.42%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int left , right;
-        left = nums[0];
-        for (int i=1; i<nums.size(); i++) {
-            right = nums[i];
-            if (left == right)
+        //two pointers
+        // sort(nums.begin(), nums.end());
+        // int left , right;
+        // left = nums[0];
+        // for (int i=1; i<nums.size(); i++) {
+        //     right = nums[i];
+        //     if (left == right)
+        //         return true;
+        //     else;
+        //         left = nums[i];
+        // }
+        // return false;
+        
+        // brute force
+        // for (int i=0; i<nums.size(); i++) {
+        //     for (int j=i+1; j<nums.size(); j++){
+        //         if (nums[i] == nums[j])
+        //             return true;
+        //     }
+        // }
+        // return false;
+        unordered_set<int> s;
+        for (int i : nums) {
+            if (s.find(i) != s.end())
                 return true;
-            else;
-                left = nums[i];
+            else 
+                s.insert(i);
         }
         return false;
     }
