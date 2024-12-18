2394a0d1232fac33a3a8e97c52b877603885cff3
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-18 12:46:19 +0500

    Time: 3 ms (77.86%) | Memory: 14.8 MB (18.78%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Brute Force
+        // for (int i=0; i<nums.size(); i++) {
+        //     for (int j=0; j<nums.size(); j++) {
+        //         if (i == j) 
+        //             continue;
+        //         if ((nums[i] + nums[j]) == target)
+        //             return {i, j};
+        //     }
+        // }
+        // return {};
+        
+        //Map
+        unordered_map<int, int> map;
         for (int i=0; i<nums.size(); i++) {
-            for (int j=0; j<nums.size(); j++) {
-                if (i == j) 
-                    continue;
-                if ((nums[i] + nums[j]) == target)
-                    return {i, j};
+            int diff = target - nums[i];
+            if (map.find(diff) != map.end()) {
+                return {map[diff], i};
             }
+            map[nums[i]] = i;
         }
+
         return {};
     }
 };
