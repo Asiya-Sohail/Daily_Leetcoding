da6001afce9e95477adb66ae557112ab5f326052
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-21 14:03:39 +0500

    Time: 5 ms (34.70%) | Memory: 18 MB (23.03%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         // Heap & Map Approach
-        // map<int, int> mp;
-        // priority_queue<pair<int, int>> pq;
-        // for (int n : nums) 
-        //     mp[n]++;
-        // for (auto p : mp)
-        //     pq.push({p.second, p.first});
-        // vector<int> ans;
-        // for (int i=0; i<k; i++){
-        //     ans.push_back(pq.top().second);
-        //     pq.pop();
-        // }
-        // return ans;
-        
-        // Vector & Map Approach
         map<int, int> mp;
+        priority_queue<pair<int, int>> pq;
         for (int n : nums) 
             mp[n]++;
-        vector<pair<int, int>> vec(mp.begin(), mp.end());
-        sort(vec.begin(), vec.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
-            return a.second > b.second;
-        });
+        for (auto p : mp)
+            pq.push({p.second, p.first});
         vector<int> ans;
-        for (int i=0; i<k; i++) {
-            ans.push_back(vec[i].first);
+        for (int i=0; i<k; i++){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
         return ans;
+        
+        // Vector & Map Approach
+        // map<int, int> mp;
+        // for (int n : nums) 
+        //     mp[n]++;
+        // vector<pair<int, int>> vec(mp.begin(), mp.end());
+        // sort(vec.begin(), vec.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
+        //     return a.second > b.second;
+        // });
+        // vector<int> ans;
+        // for (int i=0; i<k; i++) {
+        //     ans.push_back(vec[i].first);
+        // }
+        // return ans;
     }
 };
