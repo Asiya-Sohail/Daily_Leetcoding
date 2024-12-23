bc267c4b0f9133f92805bde316824fa3f9d56b38
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-23 23:59:02 +0500

    Time: 4 ms (54.20%) | Memory: 25.8 MB (27.03%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_set<char>> row(9), col(9), square(9);
+//          Vector Set Approach        
+//         vector<unordered_set<char>> row(9), col(9), square(9);
+//         for (int i=0; i<9; i++) {
+//             for (int j=0; j<9; j++) {
+//                 char num = board[i][j];
+//                 if (num == '.') 
+//                     continue;
+
+//                 if (row[i].find(num) != row[i].end())
+//                     return false;
+//                 row[i].insert(num);
+
+//                 if (col[j].find(num) != col[j].end())
+//                     return false;
+//                 col[j].insert(num);
+
+//                 int ind = (i/3) * 3 + (j/3);
+//                 if (square[ind].find(num) != square[ind].end())
+//                     return false;
+//                 square[ind].insert(num);
+
+//             }
+//         }
+//         return true;
+        
+        //Only Set Approach
+        unordered_set<char> row[9], col[9], square[9];
         for (int i=0; i<9; i++) {
             for (int j=0; j<9; j++) {
                 char num = board[i][j];
                 if (num == '.') 
                     continue;
-
-                if (row[i].find(num) != row[i].end())
-                    return false;
-                row[i].insert(num);
-
-                if (col[j].find(num) != col[j].end())
-                    return false;
-                col[j].insert(num);
-
                 int ind = (i/3) * 3 + (j/3);
-                if (square[ind].find(num) != square[ind].end())
+                if (row[i].count(num) || col[j].count(num) 
+                || square[ind].count(num))
                     return false;
+                
+                row[i].insert(num);
+                col[j].insert(num);
                 square[ind].insert(num);
-
             }
         }
         return true;
