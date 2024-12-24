01e5c4ae8278a952accdb1079219cf62ea7dffbf
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-24 13:26:31 +0500

    Time: 43 ms (76.71%) | Memory: 51.5 MB (76.26%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         //Brute Force Approch (Not accepted)
-//         Use Heap DS
-//         if (nums.empty()) 
-//             return 0;
-//         priority_queue<int, vector<int>, greater<int>> pq(nums.begin(), nums.end());
-//         int longest = 1, current = 1;
-//         int prev = pq.top();
-//         pq.pop();
-//         while (! pq.empty()) {
-//             int curr = pq.top(); pq.pop();
-//             if (curr == prev+1)
-//                 current++;
-//             else if (curr != prev) {
-//                 longest = max(longest, current);
-//                 current = 1;
-//             }
-//             prev = curr;
-//         }
+        // Use Heap DS
+        if (nums.empty()) 
+            return 0;
+        priority_queue<int, vector<int>, greater<int>> pq(nums.begin(), nums.end());
+        int longest = 1, current = 1;
+        int prev = pq.top();
+        pq.pop();
+        while (! pq.empty()) {
+            int curr = pq.top(); pq.pop();
+            if (curr == prev+1)
+                current++;
+            else if (curr != prev) {
+                longest = max(longest, current);
+                current = 1;
+            }
+            prev = curr;
+        }
         
-//         return max(longest, current);
+        return max(longest, current);
         
         // Use Unordered_map (solved but not efficient)
         // unordered_map<int, bool> mp;
@@ -45,22 +45,22 @@ public:
         // return maxlen;
         
         //Use unordered set
-        unordered_set<int> set;
-        for (int i : nums) {
-            set.insert(i);
-        } 
-        int longest = 0;
-        for (int i : nums) {
-            if (set.find(i - 1) == set.end()){ //No shorter number found (Mean start of series)
-                int curr = i; //current number
-                int current = 1; //current sequence length
-                while (set.find(curr + 1) != set.end()) {
-                    curr++;
-                    current++;
-                }
-                longest = max(longest, current);
-            }
-        }
-        return longest;
+        // unordered_set<int> set;
+        // for (int i : nums) {
+        //     set.insert(i);
+        // } 
+        // int longest = 0;
+        // for (int i : nums) {
+        //     if (set.find(i - 1) == set.end()){ //No shorter number found (Mean start of series)
+        //         int curr = i; //current number
+        //         int current = 1; //current sequence length
+        //         while (set.find(curr + 1) != set.end()) {
+        //             curr++;
+        //             current++;
+        //         }
+        //         longest = max(longest, current);
+        //     }
+        // }
+        // return longest;
     }
 };
