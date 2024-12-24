452107c9935ee78640af9bd797d0e357e9b476bd
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-24 13:54:07 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (27.23%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,5 +5,18 @@ public:
             return haystack.find(needle);
         else 
             return -1;
+        
+        //Error
+    //     int k , j;
+    //     for (int i=0; i<= haystack.size() - needle.size(); i++) {
+    //         k = 0;
+    //         for (j = i; k<needle.size(); j++, k++) {
+    //             if (haystack[j] != needle[k])
+    //                 break;
+    //         }                
+    //         if (k == needle.size())
+    //             return i;   
+    //     }
+    //     return -1;
     }
 };
