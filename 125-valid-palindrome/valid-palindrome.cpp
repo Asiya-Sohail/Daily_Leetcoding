116631e63c69b827c6fd70781d1748137984e372
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-26 00:58:04 +0500

    Time: 3 ms (29.07%) | Memory: 10.8 MB (5.31%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,22 +2,35 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         // Two pointers Approach
-        int left = 0, right = s.size() - 1;
-        while (left < right) {
-            while (! isalnum(s[left]) && left < right) {
-                left++; 
-            }
+//         int left = 0, right = s.size() - 1;
+//         while (left < right) {
+//             while (! isalnum(s[left]) && left < right) {
+//                 left++; 
+//             }
 
-            while (! isalnum(s[right]) && left < right) {
-                right--;
-            }
+//             while (! isalnum(s[right]) && left < right) {
+//                 right--;
+//             }
 
-            if (tolower(s[left]) != tolower(s[right]))
-                return false;
-            else {
-                left++; right--;
-            }    
+//             if (tolower(s[left]) != tolower(s[right]))
+//                 return false;
+//             else {
+//                 left++; right--;
+//             }    
+//         }
+//         return true;
+
+        //Reverse Function approach
+        string str;
+        for (char c : s) {
+            if (isalnum(c))
+                str += tolower(c);
         }
-        return true;
+        string temp = str;
+        reverse(temp.begin(), temp.end());
+        if (str != temp)
+            return false;
+        else 
+            return true;
     }
 };
