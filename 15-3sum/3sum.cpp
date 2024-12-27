3f992a769359bda470f11c30f18c06bbd869a3e2
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-28 02:10:48 +0500

    Time: 47 ms (74.61%) | Memory: 29 MB (64.48%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        //Two Pointers Approach
-        int target = 0, sum;
-        set<vector<int>> ans;
+        //Two Pointers Approach (Hash Set Approach)
+        // int target = 0, sum;
+        // set<vector<int>> ans;
+        // sort(nums.begin(), nums.end());
+        // for (int i=0; i<nums.size(); i++) {
+        //     int j = i + 1, k = nums.size() - 1;
+        //     while (j < k) {
+        //         sum = nums[i] + nums[j] + nums[k];
+        //         if (sum == target) {
+        //             ans.insert({nums[i] , nums[j] , nums[k]});
+        //             j++; k--;
+        //         }
+        //         else if (sum > target) 
+        //             k--;
+        //         else 
+        //             j++;
+        //     }
+        // }
+        // vector<vector<int>> op;
+        // for (auto i : ans) 
+        //     op.push_back(i);
+        // return op;
+
+        //without set approach
         sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
         for (int i=0; i<nums.size(); i++) {
             int j = i + 1, k = nums.size() - 1;
             while (j < k) {
-                sum = nums[i] + nums[j] + nums[k];
-                if (sum == target) {
-                    ans.insert({nums[i] , nums[j] , nums[k]});
-                    j++; k--;
-                }
-                else if (sum > target) 
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0) 
+                   j++;
+                else if (sum > 0)
                     k--;
-                else 
-                    j++;
+                else {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    int temp1 = j, temp2 = k;
+                    while (j < k && nums[j] == nums[temp1]) j++;
+                    while (j < k && nums[k] == nums[temp2]) k--;
+                }
             }
+            while (i+1 < nums.size() && nums[i] == nums[i+1]) i++;
         }
-        vector<vector<int>> op;
-        for (auto i : ans) 
-            op.push_back(i);
-        return op;
+        return ans;
     }
 };
