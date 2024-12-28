701f0a109cd89a7378ab3e8be4591ee91c33fb27
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-28 11:34:42 +0500

    Time: 0 ms (100.00%) | Memory: 9.1 MB (33.79%) - LeetSync

--- 228-summary-ranges/summary-ranges.cpp
+++ 228-summary-ranges/summary-ranges.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
+        // One Approach
+        // vector<string> ans;
+        // if (nums.empty()) return ans;
+
+        // int curr = 0;
+        // for (int i=1; i<=nums.size(); i++) {
+        //     if (i == nums.size() || nums[i-1] + 1 != nums[i]){
+        //         if (curr == i - 1)
+        //             ans.push_back(to_string(nums[curr]));
+        //         else 
+        //             ans.push_back(to_string(nums[curr]) + "->" + to_string(nums[i-1]));
+        //         curr = i;
+        //     }
+        // }
+        // return ans;
+
         vector<string> ans;
         if (nums.empty()) return ans;
 
-        int curr = 0;
+        int curr = nums[0];
         for (int i=1; i<=nums.size(); i++) {
             if (i == nums.size() || nums[i-1] + 1 != nums[i]){
-                if (curr == i - 1)
-                    ans.push_back(to_string(nums[curr]));
+                if (curr == nums[i - 1])
+                    ans.push_back(to_string(curr));
                 else 
-                    ans.push_back(to_string(nums[curr]) + "->" + to_string(nums[i-1]));
-                curr = i;
+                    ans.push_back(to_string(curr) + "->" + to_string(nums[i-1]));
+                if (i < nums.size())
+                    curr = nums[i];
             }
         }
         return ans;
