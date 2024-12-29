9a3e72e6ab0b8505de4208f60ddafe9b5f0044a7
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-29 21:02:20 +0500

    Time: 83 ms (5.08%) | Memory: 20 MB (91.91%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,9 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        #Brute Force Approach
+        nums.sort()
+        for i in range(1, len(nums)):
+            if (nums[i-1] == nums[i]):
+                return True
+        return False
         # Set Approach
-        if (len(nums) != len(set(nums))):
-            return True
-        else: 
-            return False
+        # if (len(nums) != len(set(nums))):
+        #     return True
+        # else: 
+        #     return False
         
