193ba6387c25baf917f0450b3145c3f4786166fe
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-29 16:21:03 +0500

    Time: 0 ms (100.00%) | Memory: 27.3 MB (17.10%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -18,24 +18,46 @@ public:
         // }
         // return res;
 
-        //Prefix and Suffix Sum Arrays
+        //Prefix and Suffix Arrays (Approach 2) 
+        // if (height.empty())
+        //     return 0;
+        // int res = 0;
+        // vector<int> left(height.size());
+        // vector<int> right(height.size());
+
+        // int max_no = INT_MIN; 
+        // for (int i=0; i<height.size(); i++) {
+        //     if (max_no < height[i])
+        //         max_no = height[i];
+        //     left[i] = max(max_no, height[i]);
+        // }
+        // max_no = INT_MIN;
+        // for (int i = height.size() - 1; i >= 0; i--) {
+        //     if (max_no < height[i])
+        //         max_no = height[i];
+        //     right[i] = max(max_no, height[i]);
+        // }
+
+        // for (int i=0; i<height.size(); i++) {
+        //     res += min(left[i], right[i]) - height[i];
+        // }
+
+        // return res;
+
+        // Prefix and Suffix Sum Arrays (Approach 3) 
         if (height.empty())
             return 0;
         int res = 0;
         vector<int> left(height.size());
         vector<int> right(height.size());
 
-        int max_no = INT_MIN; 
-        for (int i=0; i<height.size(); i++) {
-            if (max_no < height[i])
-                max_no = height[i];
-            left[i] = max(max_no, height[i]);
+        left[0] = height[0];
+        for (int i=1; i<height.size(); i++) {
+            left[i] = max(left[i-1], height[i]);
         }
-        max_no = INT_MIN;
-        for (int i = height.size() - 1; i >= 0; i--) {
-            if (max_no < height[i])
-                max_no = height[i];
-            right[i] = max(max_no, height[i]);
+        right[height.size()-1] = height[height.size()-1];
+        for (int i = height.size() - 2; i >= 0; i--) {
+            right[i] = max(right[i+1], height[i]);
         }
 
         for (int i=0; i<height.size(); i++) {
