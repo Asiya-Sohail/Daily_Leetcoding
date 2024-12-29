494545533eec4edfd6b03fecd48db8ffeb4a22ec
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-29 22:53:53 +0500

    Time: 12 ms (58.44%) | Memory: 25.9 MB (15.93%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,15 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        #Brute Force Approach
-        nums.sort()
-        for i in range(1, len(nums)):
-            if (nums[i-1] == nums[i]):
-                return True
-        return False
+        #Sorting Approach
+        # nums.sort()
+        # for i in range(1, len(nums)):
+        #     if (nums[i-1] == nums[i]):
+        #         return True
+        # return False
+
+        #Hash Map Approach
+        # freq = {} # Initialize an empty hash map
+        # for i in nums :
+        #     if i in freq:
+        #         return True
+        #     freq[i] = 1
+        # return False
+
         # Set Approach
-        # if (len(nums) != len(set(nums))):
-        #     return True
-        # else: 
-        #     return False
+        if (len(nums) != len(set(nums))):
+            return True
+        else: 
+            return False
+
         
