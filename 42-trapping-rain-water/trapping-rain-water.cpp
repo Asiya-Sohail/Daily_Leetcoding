4f8d0574e4584cf5017adfcb562c79948b00506c
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-29 16:34:50 +0500

    Time: 0 ms (100.00%) | Memory: 26 MB (35.01%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -45,25 +45,46 @@ public:
         // return res;
 
         // Prefix and Suffix Sum Arrays (Approach 3) 
+        // if (height.empty())
+        //     return 0;
+        // int res = 0;
+        // vector<int> left(height.size());
+        // vector<int> right(height.size());
+
+        // left[0] = height[0];
+        // for (int i=1; i<height.size(); i++) {
+        //     left[i] = max(left[i-1], height[i]);
+        // }
+        // right[height.size()-1] = height[height.size()-1];
+        // for (int i = height.size() - 2; i >= 0; i--) {
+        //     right[i] = max(right[i+1], height[i]);
+        // }
+
+        // for (int i=0; i<height.size(); i++) {
+        //     res += min(left[i], right[i]) - height[i];
+        // }
+
+        // return res;
+
+        //Two Pointers Approach (Approach 4)
         if (height.empty())
             return 0;
         int res = 0;
-        vector<int> left(height.size());
-        vector<int> right(height.size());
-
-        left[0] = height[0];
-        for (int i=1; i<height.size(); i++) {
-            left[i] = max(left[i-1], height[i]);
+        int l = 0, r = height.size() - 1;
+        int left = height[l];
+        int right = height[r];
+        while (l < r) {
+            if (left < right){
+                l++;
+                left = max(left, height[l]);
+                res += left - height[l];
+            }
+            else {
+                r--;
+                right = max(right, height[r]);
+                res += right - height[r];
+            }
         }
-        right[height.size()-1] = height[height.size()-1];
-        for (int i = height.size() - 2; i >= 0; i--) {
-            right[i] = max(right[i+1], height[i]);
-        }
-
-        for (int i=0; i<height.size(); i++) {
-            res += min(left[i], right[i]) - height[i];
-        }
-
         return res;
     }
 };
