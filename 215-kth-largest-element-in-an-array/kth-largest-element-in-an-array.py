34fb3333d09ad47253e06eccf153d63118521c9e
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2024-12-30 22:35:02 +0500

    Time: 55 ms (73.25%) | Memory: 28.7 MB (17.90%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,4 +1,5 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         nums.sort()
-        return nums[-k]
+        # return nums[-k]
+        return nums[len(nums) - k ]
