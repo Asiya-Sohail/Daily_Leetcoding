fcf0c8a26d001904e1132108126d025b95e27d1a
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2025-07-29 00:19:55 +0500

    Time: 0 ms (100.00%) | Memory: 12.9 MB (57.40%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -5,15 +5,29 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        length = len(nums)
-        for index, value in enumerate(nums):
-            if target == value:
-                return index
-            elif index < length - 1:
-                if value < target and  nums[index+1] > target:
-                    return index + 1
+        # length = len(nums)
+        # for index, value in enumerate(nums):
+        #     if target == value:
+        #         return index
+        #     elif index < length - 1:
+        #         if value < target and  nums[index+1] > target:
+        #             return index + 1
+        #     else:
+        #         if nums[length - 1] < target:
+        #             return len(nums)
+        #         else :
+        #             return 0
+        
+        # Binary Search
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                if nums[length - 1] < target:
-                    return len(nums)
-                else :
-                    return 0
+                right = mid - 1
+        return left
+
