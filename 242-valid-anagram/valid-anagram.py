7fad61b5d55d6d29a4fd9576bc34527ceafd309e
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2026-05-06 12:10:15 +0500

    Time: 19 ms (66.21%) | Memory: 12.4 MB (86.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,8 +5,13 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if sorted(s)==sorted(t):
-            return True
-        else:
-            return False
+        freq_s = {}
+        freq_t = {}
+        for i in s:
+            freq_s[i] = freq_s.get(i, 0) + 1
+        for i in t:
+            freq_t[i] = freq_t.get(i, 0) + 1
+        return freq_s == freq_t
+        
+        
         
