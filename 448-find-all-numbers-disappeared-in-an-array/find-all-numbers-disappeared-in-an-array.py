b7a37ecab93d4b6066caef35d7bebc84927abd11
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2026-05-09 15:32:47 +0500

    Time: 32 ms (72.36%) | Memory: 26.8 MB (77.88%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -5,15 +5,23 @@ class Solution(object):
         :rtype: List[int]
         """
         # return [i for i in range(1, len(nums)+1) if i not in nums] O(n^2)
-        check = {}
+        
+        # check = {}
+        # for i in range(1, len(nums)+1):
+        #     check[str(i)] = 0
+
+        # for i in nums:
+        #     check[str(i)] += 1
+
+        # return [int(key) for key, val in check.items() if val == 0 ]
+        n = len(nums)
         res = []
-        for i in range(1, len(nums)+1):
-            check[str(i)] = 0
 
-        for i in nums:
-            check[str(i)] += 1
-
-        return [int(key) for key, val in check.items() if val == 0 ]
+        check = set(nums)
+        for i in range(1, n+1):
+            if i not in check:
+                res.append(i)
+        return res
 
         
 
