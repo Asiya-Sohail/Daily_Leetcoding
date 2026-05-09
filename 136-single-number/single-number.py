099cd6249c5e6cf76d99226741bab1dab8cae31d
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2026-05-09 15:55:02 +0500

    Time: 2 ms (66.81%) | Memory: 13.8 MB (79.04%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -7,12 +7,17 @@ class Solution(object):
         if len(nums) <= 2:
             return nums[0]
 
-        nums.sort()
-        i=0
-        while  (i < len(nums)-1):
-            if nums[i] != nums[i+1]:
-                return nums[i]
-            else:
-                i += 1
-            i+=1
-        return nums[-1]
+        # nums.sort()
+        # i=0
+        # while  (i < len(nums)-1):
+        #     if nums[i] != nums[i+1]:
+        #         return nums[i]
+        #     else:
+        #         i += 1
+        #     i+=1
+        # return nums[-1]
+
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
