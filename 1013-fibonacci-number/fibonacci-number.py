1edda3f77a5e3d4925d52720743d4361614433b9
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2026-05-11 10:33:46 +0500

    Time: 613 ms (30.30%) | Memory: 12.4 MB (16.57%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -6,14 +6,5 @@ class Solution(object):
         """
         if n <= 1:
             return n
-
-        # fibonacci = [0, 1]
-        # for i in range(2, n+1):
-        #     fibonacci.append(fibonacci[i-1] + fibonacci[i-2])
-
-        # return fibonacci[n]
+        return self.fib(n-1) + self.fib(n-2)
         
-        a, b = 0, 1
-        for _ in range(2, n+1):
-            a, b = b, a+b
-        return b
