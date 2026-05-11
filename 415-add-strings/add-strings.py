38543593a7f7b783be5dfad0de2fb2d7ec219e13
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2026-05-11 09:55:36 +0500

    Time: 14 ms (16.78%) | Memory: 12.5 MB (20.14%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -5,4 +5,20 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        return str(int(num1) + int(num2))
+        
+        i, j = len(num1)-1, len(num2)-1
+        carry = 0
+        result = []
+        while i>=0 or j >=0 or carry:
+            total = carry
+            if i>=0:
+                total += (ord(num1[i])-ord('0'))
+                i -= 1
+            if j >=0:
+                total += (ord(num2[j])-ord('0'))
+                j -= 1
+            
+            result.append(str(total % 10))
+            carry = total // 10
+        return ''.join(result[::-1])
+        
