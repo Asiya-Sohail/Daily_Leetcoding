c471395be920d693feb30a9937578f0ffbbfe7e2
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2026-05-12 06:59:20 +0500

    Time: 3 ms (18.80%) | Memory: 12.3 MB (53.51%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -4,6 +4,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        binary = bin(n)
-        return binary[2:].count('1')
-        
+        # binary = bin(n)
+        # return binary[2:].count('1')
+        count = 0
+        while n:
+            if n%2 == 1:
+                count += 1
+            n //= 2
+        return count
