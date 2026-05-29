026a34cb66b618d82b4675e05e229aed71946e93
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2026-05-30 00:11:26 +0500

    Time: 39 ms (19.28%) | Memory: 12.5 MB (57.98%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for i in nums1:
+            idx = nums2.index(i)
+            found = False
+            for j in range(idx+1, len(nums2)):
+                if nums2[j] > i:
+                    ans.append(nums2[j])
+                    found = True
+                    break
+            if not found:
+                ans.append(-1)
+        return ans
+            
