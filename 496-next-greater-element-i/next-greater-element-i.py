772dd695a5363e06105d2e40204e5d2dd83e8e07
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2026-06-10 23:19:58 +0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (92.02%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -5,16 +5,23 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        ans = []
-        for i in nums1:
-            idx = nums2.index(i)
-            found = False
-            for j in range(idx+1, len(nums2)):
-                if nums2[j] > i:
-                    ans.append(nums2[j])
-                    found = True
-                    break
-            if not found:
-                ans.append(-1)
-        return ans
-            
+        # ans = []
+        # for i in nums1:
+        #     idx = nums2.index(i)
+        #     found = False
+        #     for j in range(idx+1, len(nums2)):
+        #         if nums2[j] > i:
+        #             ans.append(nums2[j])
+        #             found = True
+        #             break
+        #     if not found:
+        #         ans.append(-1)
+        # return ans
+        ng = {}
+        st = []
+        for num in reversed(nums2):
+            while st and st[-1] <= num:
+                st.pop()
+            ng[num] = -1 if not st else st[-1]
+            st.append(num)
+        return [ng[num] for num in nums1]
