dcdb8a55c2a9454ea7d730fe4984927a52212709
Asiya Sohail <139541728+Asiya-Sohail@users.noreply.github.com>
2026-06-11 18:52:28 +0500

    Time: 20 ms (51.16%) | Memory: 12.4 MB (52.11%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,10 +1,19 @@
 class Solution(object):
+    def f(self, n, r):
+        if r[n] > 0:
+            return r[n]
+        if n <= 1:
+            r[n] = n
+            return n
+        else:
+            r[n] = self.f(n-1, r) + self.f(n-2, r)
+            return r[n]
+
     def fib(self, n):
         """
         :type n: int
         :rtype: int
         """
-        if n <= 1:
-            return n
-        return self.fib(n-1) + self.fib(n-2)
+        r = [0] * (n+1)
+        return self.f(n, r)
         
